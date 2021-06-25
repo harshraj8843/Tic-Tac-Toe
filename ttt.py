cddba86cac18b34d2116e3bd032586a7606b96bb
Harsh Raj <harshraj8843@gmail.com>
2021-06-25 15:35:13 +0530

    initial commit

--- ttt.py
+++ ttt.py
@@ -3,10 +3,10 @@ import random
 from itertools import combinations
 
 # all numbers allowed for input
-num_array = [1,2,3,4,5,6,7,8,9]
+num_list = [1,2,3,4,5,6,7,8,9]
 
-# wining array
-win_array = [
+# wining list
+win_list = [
     [1,2,3],
     [1,4,7],
     [1,5,9],
@@ -17,26 +17,26 @@ win_array = [
     [7,8,9]
 ]
 
-# no chance array
-no_array = []
+# no chance list
+no_list = []
 
-# list of reserved array or input chosed
-res_array = []
+# list of reserved list or input chosed
+res_list = []
 
-# user input array
-user_array = []
+# user input list
+user_list = []
 
-# computer input array
-com_array = []
+# computer input list
+com_list = []
 
 # print tic-tac-toe matrix based on user and computer input
 def print_matrix():
     print()
     for i in range(1,10):
-        if i in user_array:
+        if i in user_list:
             print('1', end=' ')
         else:
-            if i in com_array:
+            if i in com_list:
                 print('0', end=' ')
             else:
                 print('-', end=' ')
@@ -58,21 +58,21 @@ def sub_list (list):
 
 # user move analysis
 def user_move_analysis():
-    if len(user_array) == 2:
-        for i in win_array:
-            check = all(item in i for item in user_array)
+    if len(user_list) == 2:
+        for i in win_list:
+            check = all(item in i for item in user_list)
             if check is True:
-                if i in no_array:
+                if i in no_list:
                     pass
                 else:
                     return i
-    if len(user_array) > 2:
-        user_move_array = sub_list(user_array)
-        for i in user_move_array:
-            for j in win_array:
+    if len(user_list) > 2:
+        user_move_list = sub_list(user_list)
+        for i in user_move_list:
+            for j in win_list:
                 check = all(item in j for item in i)
                 if check is True:
-                    if j in no_array:
+                    if j in no_list:
                         pass
                     else:
                         return j
@@ -80,35 +80,35 @@ def user_move_analysis():
 # for user_input
 def user_minus(fav_list):
     for i in fav_list:
-        if i in user_array:
+        if i in user_list:
             pass
         else:
             return i
 
-# computer self analysis array function
-def self_analysis_array():
-    if(len(com_array) == 1):
-        for i in win_array:
-            check = all(item in i for item in com_array)
+# computer self analysis list function
+def self_analysis_list():
+    if(len(com_list) == 1):
+        for i in win_list:
+            check = all(item in i for item in com_list)
             if check is True:
                 flag = False
                 for j in i:
-                    if j in user_array:
+                    if j in user_list:
                         flag = True
                         break
                 if(flag == True):
                     pass
                 else:
                     return i
-    if len(com_array) > 1:
-        com_move_array = sub_list(com_array)
-        for i in com_move_array:
-            for j in win_array:
+    if len(com_list) > 1:
+        com_move_list = sub_list(com_list)
+        for i in com_move_list:
+            for j in win_list:
                 check = all(item in j for item in i)
                 if check is True:
                     flag = False
                     for k in j:
-                        if k in user_array:
+                        if k in user_list:
                             flag = True
                             break
                     if(flag == True):
@@ -118,81 +118,81 @@ def self_analysis_array():
 
 def self_minus(fav_list):
     for i in fav_list:
-        if i in com_array:
+        if i in com_list:
             pass
         else:
             return i
 
 def minus():
-    for i in num_array:
-        if i in res_array:
+    for i in num_list:
+        if i in res_list:
             pass
         else:
             return i
 
 def self_analysis():
-    favourable_array = self_analysis_array()
-    if favourable_array is None:
+    favourable_list = self_analysis_list()
+    if favourable_list is None:
         fav_input = minus()
-        com_array.append(fav_input)
-        res_array.append(fav_input)
+        com_list.append(fav_input)
+        res_list.append(fav_input)
         print('Computer Input (0) => ', fav_input)
     else:
-        fav_input = self_minus(favourable_array)
-        com_array.append(fav_input)
-        res_array.append(fav_input)
+        fav_input = self_minus(favourable_list)
+        com_list.append(fav_input)
+        res_list.append(fav_input)
         print('Computer Input (0) => ', fav_input)
 
 # user input function
 def user_input():
     temp = int(input('User Input (1) => '))
 
-    if temp not in num_array:
+    if temp not in num_list:
         print('*** Error in Input ***')
         user_input()
     else:
-        if(temp in res_array):
+        if(temp in res_list):
             print('*** Reserved ***')
             user_input()
         else:
-            user_array.append(temp)
-            res_array.append(temp)
+            user_list.append(temp)
+            res_list.append(temp)
 
 # computer first time input function
 def computer_first_input():
     # most probable numbers to start to win
     temp = random.choice([1,2,3,4,7])
-    com_array.append(temp)
-    res_array.append(temp)
+    com_list.append(temp)
+    res_list.append(temp)
     print('Computer Input (0) => ', temp)
 
 def computer_input():
-    if(len(user_array) > 1):
+    if(len(user_list) > 1):
         # user move analysis
-        favourable_array = user_move_analysis()
-        if favourable_array is None:
+        favourable_list = user_move_analysis()
+        if favourable_list is None:
             # self move analysis
             self_analysis()
         else:
-            fav_input = user_minus(favourable_array)
-            if fav_input in res_array:
+            fav_input = user_minus(favourable_list)
+            if fav_input in res_list:
                 # self move analysis
                 self_analysis()
             else:
-                com_array.append(fav_input)
-                res_array.append(fav_input)
-                no_array.append(favourable_array)
+                com_list.append(fav_input)
+                res_list.append(fav_input)
+                no_list.append(favourable_list)
                 print('Computer Input (0) => ', fav_input)
     else:
         # self move analysis
         self_analysis()
 
 def match_computer():
-    comb = list(combinations(com_array,3))
+    comb = list(combinations(com_list,3))
     arr = []
     for ii in comb:
         arr.append(list(ii))
-    for i in win_array:
+    for i in win_list:
         for j in arr:
             check = all(item in i for item in j)
             if check is True:
@@ -201,11 +201,11 @@ def match_computer():
                 pass
 
 def match_user():
-    comb = list(combinations(user_array,3))
+    comb = list(combinations(user_list,3))
     arr = []
     for ii in comb:
         arr.append(list(ii))
-    for i in win_array:
+    for i in win_list:
         for j in arr:
             check = all(item in i for item in j)
             if check is True:
